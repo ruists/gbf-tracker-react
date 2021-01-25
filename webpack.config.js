17d40d9607985a3b8fa0ee405b6c218786a7123f
ruists <clovias@gmail.com>
2021-01-25 03:16:02 +0000

    Basic project setup

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+var path = require("path");
+
+var DIST_DIR = path.resolve(__dirname, "dist");
+var SRC_DIR = path.resolve(__dirname, "src");
+
+var config = {
+    entry: SRC_DIR + "/app/index.js",
+    output: {
+        path: DIST_DIR + "/app",
+        filename: "bundle.js",
+        publicPath: "/app/",
+    },
+    module: {
+        rules: [{
+            test: /\.js?/,
+            include: SRC_DIR,
+            loader: "babel-loader",
+            options: {
+                presets: ["@babel/preset-env", "@babel/preset-react"]
+            }
+        }],
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'dist'),
+        compress: true,
+        open: true,
+        port: 9000
+    }
+};
+
+module.exports = config;
