4271289f0aec883ec5af3948ab23c23ce036f717
ruists <clovias@gmail.com>
2021-01-26 00:06:03 +0000

    Added bootstrap and a basic header component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,17 @@
-console.log("It works...");
+import React from "react";
+import { render } from "react-dom";
+import { Header } from "./components/Header";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <Header />
+        </div>
+      </div>
+    );
+  }
+}
+
+render(<App />, window.document.getElementById("app"));
