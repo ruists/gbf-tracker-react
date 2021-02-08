2722976eb70194473e1e89611e127cca4edb9b12
ruists <clovias@gmail.com>
2021-02-08 18:18:38 +0000

    Added basic wrapper for an authorized fetch()

---
+++ src/app/utils/fetch.js
@@ -0,0 +1,26 @@
+import { authenticationService } from "app/services/authentication.service";
+
+export async function authorizedFetch(method, url, body) {
+  if (method != "POST" && method != "GET") {
+    return -1;
+  }
+  if (!url || url.length === 0) {
+    return -1;
+  }
+
+  const userInfo = authenticationService.currentUserValue();
+  const body = {
+    usr: userInfo.name,
+    token: userInfo,
+    ...body
+  };
+  const reqOptions = {
+    method: method,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  };
+
+  const res = await fetch(url, reqOptions);
+  const result = await res.json();
+  return { result: result, code: res.status };
+}
