5ebf1be430b93af2b765930ef0f7841ae4c01375
ruists <clovias@gmail.com>
2021-02-25 01:26:19 +0000

    Change LoginHeader form to old design due to sizing problems

--- src/app/components/authentication/LoginHeader.js
+++ src/app/components/authentication/LoginHeader.js
@@ -1,51 +1,47 @@
-import React, { useState } from "react";
-import "app/styles/auth.css";
-import { useHistory } from "react-router-dom";
+import React, { useState } from 'react'
+import 'app/styles/auth.css'
+import { useHistory } from 'react-router-dom'
 
 export function LoginHeader(props) {
-  const history = useHistory();
-  const [error, setError] = useState(false);
+  const history = useHistory()
+  const [error, setError] = useState(false)
 
   const submitLoginInput = async (e) => {
-    e.preventDefault();
-    const usr = document.getElementById("usernameLoginControl").value;
-    const pwd = document.getElementById("passwordLoginControl").value;
-    let result = await props.handleLogin(usr, pwd);
+    e.preventDefault()
+    const usr = document.getElementById('usernameLoginControl').value
+    const pwd = document.getElementById('passwordLoginControl').value
+    let result = await props.handleLogin(usr, pwd)
 
     if (result == 200) {
-      setError(false);
-      history.push("/");
+      setError(false)
+      history.push('/')
     } else if (result == 401) {
-      setError(true);
+      setError(true)
     }
-    document.getElementById("loginForm")?.reset();
-  };
+    document.getElementById('loginForm')?.reset()
+  }
 
   return (
-    <div className="me-3">
-      <form className="d-flex" onSubmit={submitLoginInput} id="loginForm">
-        <div className="form-floating">
-          <input
-            className="form-control me-2"
-            id="usernameLoginControl"
-            type="email"
-            aria-label="E-mail"
-          />
-          <label for="usernameLoginControl">Email</label>
-        </div>
-        <div className="form-floating">
-          <input
-            className="form-control me-2"
-            id="passwordLoginControl"
-            type="password"
-            aria-label="Password"
-          />
-          <label for="passwordLoginControl">Password</label>
-        </div>
-        <button className="btn btn-secondary" type="submit">
+    <div className='me-3'>
+      <form className='d-flex' onSubmit={submitLoginInput} id='loginForm'>
+        <input
+          className='form-control me-2'
+          id='usernameLoginControl'
+          type='email'
+          aria-label='Email'
+          placeholder='Email'
+        />
+        <input
+          className='form-control me-2'
+          id='passwordLoginControl'
+          type='password'
+          aria-label='Password'
+          placeholder='Password'
+        />
+        <button className='btn btn-secondary' type='submit'>
           Login
         </button>
       </form>
     </div>
-  );
+  )
 }
